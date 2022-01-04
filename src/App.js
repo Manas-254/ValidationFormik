b0cbd2406c89df484b47125eedcf19fc1645ddbb
Manaswini Jain <106534@tfcorp.com>
2022-01-04 22:06:34 +0530

    Adding files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Navbar />
+      <Switch>
+        <Route exact path='/login'>
+          <Login/>
+        </Route>
+        <Route exact path='/signup'>
+          <SignUp />
+        </Route>
+        <Route exact path='/'>
+            <Home />
+          </Route>
+      </Switch>
     </div>
+  </Router>
   );
 }
 
---
+++ src/components/SignUp.js
@@ -0,0 +1,106 @@
+import React from 'react'
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as yup from 'yup';
+import './style.css'
+import { withRouter } from 'react-router-dom'
+
+function SignUp(props) {
+
+    let initialValues = {
+        firstName: '',
+        lastName: '',
+        userName: '',
+        password: '',
+        remember: false
+    }
+
+    let validationSchema = yup.object().shape({
+        firstName:yup.string().required("Firstname is required"),
+        lastName:yup.string().required("Lastname is required"),
+        userName: yup.string().email("enter valid email").required("email is required"),
+        password: yup.string().required("password is required").min("5").max("10")
+    })
+
+    let onSubmit = (values, props) => {
+        console.log(values);
+        props.resetForm()
+    }
+    let navigateToLogin=()=>{
+        // console.log(props);
+        props.history.push('/login')
+       }
+    return (
+        <div className='container border mt-5'>
+            <Formik
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+                onSubmit={onSubmit}>
+
+                {(props) => (
+                    <Form>
+                        <div className='container'>
+                        <div className="mb-3">
+                                <label>FirstName</label>
+                                <Field
+
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Enter FirstName"
+                                    name="firstName"
+                                    autoComplete="off"
+
+                                />
+                                <p className='error'><ErrorMessage name="firstName" /></p>
+                            </div>
+
+                            <div className="mb-3">
+                                <label>Lastname</label>
+                                <Field
+
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Enter Lastname"
+                                    name="lastName"
+                                    autoComplete="off"
+
+                                />
+                                <p className='error'><ErrorMessage name="lastName" /></p>
+                            </div>
+
+                            <div className="mb-3">
+                                <label>Email</label>
+                                <Field
+
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Enter Email"
+                                    name="userName"
+                                    autoComplete="off"
+
+                                />
+                                <p className='error'><ErrorMessage name="userName" /></p>
+                            </div>
+                            <div className="mb-3">
+                                <label>Password</label>
+                                <Field
+
+                                    type="password"
+                                    className="form-control"
+                                    placeholder="Enter Password"
+                                    name="password"
+                                />
+                                <p className='error'><ErrorMessage name="password" /></p>
+
+                            </div>
+                            <button type="submit" className="btn btn-primary" >SignUp</button>
+
+                            <h4 style={{ cursor: 'pointer' }}  onClick={navigateToLogin}>Already have an Account?Login Here!</h4>
+                        </div>
+                    </Form>
+                )}
+            </Formik>
+        </div>
+    );
+}
+
+export default withRouter(SignUp)
